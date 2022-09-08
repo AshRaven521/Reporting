f2c216596ad89d85cbcc49319a7d9dc92145c824
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-09-08 12:15:44 +0300

    added structure that represents employee

---
+++ Reporting/ICSBEL/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSBEL.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Salary { get; set; }
+        public Employee(string name, string surname, string job, DateTime dateBitrth, double salary)
+        {
+            Name = name;
+            Surname = surname;
+            JobTitle = job;
+            BirthDate = dateBitrth;
+            Salary = salary;
+        }
+    }
+}
