06b28a0a93a7646562523a732ee982e23ee60a0e
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-09-10 19:53:27 +0300

    create excel class that creates report

---
+++ Reporting/ICSBEL/Utils/Excel.cs
@@ -0,0 +1,30 @@
+﻿using ClosedXML.Excel;
+using ICSBEL.DataAccessLayer;
+using System;
+using System.Threading.Tasks;
+
+namespace ICSBEL.Utils
+{
+    public static class Excel
+    {
+        public static async Task<bool> CreateReport(string path)
+        {
+            var data = await DB.GetSalariesForReport();
+            //DataSetHelper.CreateWorkbook(path, data);
+            try
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    workbook.Worksheets.Add(data);
+                    workbook.SaveAs(path);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при формировании excel файла! {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
