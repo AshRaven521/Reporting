5d045f4e5e64b2c9cad2dd337fdd05f59e15292b
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-09-10 19:53:08 +0300

    something unnecessary

--- Reporting/ICSBEL/Models/Employee.cs
+++ Reporting/ICSBEL/Models/Employee.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ICSBEL.Models
 {
     public class Employee
     {
-        public string Name { get;}
-        public string Surname { get;}
-        public string JobTitle { get;}
-        public DateTime BirthDate { get;}
-        public double Salary { get;}
+        public string Name { get; }
+        public string Surname { get; }
+        public string JobTitle { get; }
+        public DateTime BirthDate { get; }
+        public double Salary { get; }
         public Employee(string name, string surname, string job, DateTime dateBitrth, double salary)
         {
             Name = name;
